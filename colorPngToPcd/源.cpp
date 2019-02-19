b248af7c8b5505d1e23c46317c3c2f9067525e9e
Haden <admin@sunhx.cn>
2019-02-19 18:45:17 +0800

    filter and pseudo-color point cloud for colorPngToPcd
    colorPngToPcd增加滤波和伪彩色

--- colorPngToPcd/源.cpp
+++ colorPngToPcd/源.cpp
@@ -25,6 +25,7 @@
 #include <pcl\io\pcd_io.h>
 #include <pcl\point_cloud.h>
 #include <pcl\visualization\cloud_viewer.h>
+#include <pcl\filters\statistical_outlier_removal.h>
 
 using namespace std;
 using namespace cv;
@@ -154,7 +155,7 @@ void pcdSave(Mat mImageDepth)
 			float picDist = sqrt((i - imgHeight / 2.0)*(i - imgHeight / 2.0) + (j - imgWidth / 2.0)*(j - imgWidth / 2.0));	//图像上点到中心的像素点个数
 			float picAngle = atan2(i - imgHeight / 2.0, j - imgWidth / 2.0);												//图像上x,y和中心点角度关系
 			float angle = atan(sqrt((j - imgWidth / 2.0)*(j - imgWidth / 2.0) / FX / FX + (i - imgHeight / 2.0)*(i - imgHeight / 2.0) / FY / FY));
-			float dist = mImageDepth.at<ushort>(i, j) / 3000.0 * 125;				//原始图像深度
+			float dist = mImageDepth.at<ushort>(i, j) / 300.0 * 125;				//原始图像深度
 
 			PointXYZ p;
 			p.z = dist*cos(angle);									//坐标变换后的深度
@@ -165,10 +166,79 @@ void pcdSave(Mat mImageDepth)
 		}
 	}
 
+	//滤波
+	PointCloud<PointXYZ>::Ptr cloud_filtered(new PointCloud<PointXYZ>);
+	PointCloud<PointXYZ>::Ptr cloud(new PointCloud<PointXYZ>);
+
+	cloud = pointCloud.makeShared();
+	//创建滤波器对象
+	StatisticalOutlierRemoval<PointXYZ> sor;
+	//该算法迭代整个输入两次：在第一次迭代期间，它将计算每个点与其最近的k个邻居之间的平均距离。 可以使用setMeanK（）设置k的值。
+	//接下来，计算所有这些距离的平均值和标准偏差，以便确定距离阈值。
+	//距离阈值将等于：mean + stddev_mult * stddev。 可以使用setStddevMulThresh（）设置标准偏差的乘数。
+	//在下一次迭代期间，如果它们的平均相邻距离分别低于或高于该阈值，则点将被分类为内部或异常值。
+	//为每个查询点找到的邻居将在setInputCloud（）的所有点中找到，而不仅仅是由setIndices（）索引的那些点。 
+	//setIndices（）方法仅索引将作为搜索查询点迭代的点。
+	sor.setInputCloud(cloud);
+	sor.setMeanK(50);					//设置与50个邻居计算平均距离
+	sor.setStddevMulThresh(1.0);		//设置标准差放大系数
+	sor.filter(*cloud_filtered);
+
+	int max = 0;
 	//点云可视化
-	PointCloud<PointXYZ>::Ptr cloud = pointCloud.makeShared();
+	PointCloud<PointXYZRGB> cloud_color;
+	for (int i = 0; i < cloud_filtered->size(); i++)
+	{
+		PointXYZRGB p;
+		p.x = cloud_filtered->points.at(i).x;
+		p.y = cloud_filtered->points.at(i).y;
+		p.z = cloud_filtered->points.at(i).z;
+
+		if (max < p.z)
+			max = p.z;
+		if (p.z < 100)
+			continue;
+
+		p.z -= 200;
+		if (p.z < 64)
+		{
+			p.r = 255;
+			p.g = p.z*4;
+			p.b = 0;
+		}
+		else if (p.z < 128)
+		{
+			p.r = 512 - p.z * 4;
+			p.g = 255;
+			p.b = 0;
+		}
+		else if (p.z < 192)
+		{
+			p.r = 0;
+			p.g = 255;
+			p.b = p.z*4 - 512;
+		}
+		else if (p.z < 256)
+		{
+			p.r = p.z * 4 - 768;
+			p.g = 255;
+			p.b = 255;
+		}
+		else
+		{
+			p.r = 100;
+			p.g = 100;
+			p.b = 100;
+		}
+		
+
+		cloud_color.points.push_back(p);
+	}
+	printf("max is %d", max);
 	visualization::CloudViewer viewer("Cloud Viewer");
-	viewer.showCloud(cloud);
+	
+	PointCloud<PointXYZRGB>::Ptr cloud_show = cloud_color.makeShared();
+	viewer.showCloud(cloud_show);
 	while (!viewer.wasStopped())
 	{
 
@@ -176,9 +246,12 @@ void pcdSave(Mat mImageDepth)
 
 
 	//保存点云
-	pointCloud.height = imgHeight;
-	pointCloud.width = imgWidth;
-	io::savePCDFileBinary("pcl.pcd", pointCloud);
+	//cloud_filtered->height = imgHeight;
+	//cloud_filtered->width = imgWidth;
+	//io::savePCDFileBinary("pcl.pcd", *cloud_filtered);
+	cloud_color.height = 1;
+	cloud_color.width = cloud_color.size();
+	io::savePCDFileASCII("pcl.pcd", cloud_color);
 }
 
 
