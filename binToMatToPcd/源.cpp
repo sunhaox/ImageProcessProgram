5709453d5ff887f3519cb09c34d7974a3a41e882
Haden <admin@sunhx.cn>
2019-04-19 22:07:04 +0800

    fix remap bug
    修复畸变矫正线性插值噪声问题

--- binToMatToPcd/源.cpp
+++ binToMatToPcd/源.cpp
@@ -132,7 +132,7 @@ Mat imageUndist(Mat src)
 	//参数3、4：X\Y坐标重映射
 	//参数5：图像的插值方式
 	//参数6：边界填充方式
-	cv::remap(src, img, map1, map2, INTER_LINEAR);																	//畸变矫正
+	cv::remap(src, img, map1, map2, cv::INTER_NEAREST);																	//畸变矫正
 	return img.clone();
 }
 
